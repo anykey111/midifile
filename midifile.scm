1cd4c714b7bce239972e8ede9056a19f2ec40c30
rivo <devnull@localhost>
2013-09-20 13:24:10 +0400

    midifile parser draft

---
+++ midifile.scm
@@ -0,0 +1,91 @@
+(use bitstring)
+
+(define-record midifile format time-division tracks)
+
+(define (midi-file-open name)
+  (bitmatch (read-all name)
+    ((("MThd") (#x00000006 32) (rest bitstring))
+     (parse-midi-format rest))
+    (else
+     (error "invalid midi file signature"))))
+
+(define (parse-midi-format data)
+  (bitmatch data
+    (((format 16) (num-tracks 16) (time-division 16 bitstring) (tracks bitstring))
+     (if (> format 2)
+        (error "unsupported midi format" format))
+     (unless (and (= format 0) (= num-tracks))
+       (error "this midi format support only one track"))
+     (unless (and (= format 1) (> num-tracks 1))
+       (error "this midid format should have thow or more tracks"))
+     (parse-midi-tracks num-tracks tracks (parse-midi-time-format timi-division)))
+    (else
+     (error "invalid midi file header"))))
+
+(define (parse-midi-time-format time-division)
+  (bitmatch 
+    (((0 1) (ticks 15))
+     (list 'ticks-per-beat ticks))
+    (((1 1) (frames 7) (clocks 8))
+     (list 'frames-per-second frames clocks))))
+
+(define (parse-midi-tracks num-tracks tracks time-format)
+  (let loop ((i 1)
+             (data tracks)
+             (acc (list)))
+    (bitmatch data
+      ((("MTrk") (track-size 32) (track-data (* 8 track-size)) (rest bitstring))
+       (if (< i num-tracks)
+           (loop (add1 i)
+                 (cons (parse-midi-track-events track-data) acc)
+                 rest)
+           (reverse acc)))
+      (else
+       (error "invalid track header" i)))))
+
+(define (shift-delta-time delta d)
+  (bitwise-ior (arithmetic-shift 7 arithmetic-shif) d))
+
+(define (parse-delta-time stream delta)
+  (bitmatch stream
+    (((0 1) (d 7) (rest bitstring))
+     (valules rest (shift-delta-value detla d)))
+    (((1 1) (d 7) (rest bitstring))
+     (parse-delta-time rest (shift-delta-time delta d)))))
+
+(define (parse-midi-track-events track-data)
+  (let loop ((acc (list))
+             (data track-data))
+    (receive (rest delta)
+             (parse-delta-time data 0)
+      (bitmatch rest
+        (()
+         (reverse acc))
+        (((#x8 4) (channel 4) (note 8) (velocity 8) (rest bitstring))
+         (loop (cons (list delta 'note-off channel note velocity) acc) rest))
+        (((#x9 4) (channel 4) (note 8) (velocity 8) (rest bitstring))
+         (loop (cons (list delta 'note-on channel note velocity) acc) rest))
+        (((#xA 4) (channel 4) (note 8) (value 8) (rest bitstring))
+         (loop (cons (list delta 'note-aftertouch channel note value)) rest))
+        (((#xB 4) (channel 4) (number 8) (value 8) (rest bitstring))
+         (loop (cons (list delta 'controller channel number value)) rest))
+        (((#xC 4) (channel 4) (number 8) (unused 8) (rest bitstring))
+         (loop (cons (list delta 'program-change channel number)) rest))
+        (((#xD 4) (channel 4) (value 8) (unused 8) (rest bitstring))
+         (loop (cons (list delta 'channel-aftertouch channel value)) rest))
+        (((#xE 4) (channel 4) (value 16) (rest bitstring))
+         (loop (cons (list delta 'pitch-bend channel value)) rest))
+        (((#xFF) (data bitstring))
+         (receive (rest meta-event)
+                  (parse-midi-meta-event delta data)
+           (loop (cons meta-event acc) rest)))
+        (((#xF0) (data bitstring))
+         (receive (rest sysex-event)
+                  (parse-midi-sysex-event delta data)
+           (loop (cons sysex-event acc) rest)))
+        (((#xF7) (data bitstring))
+         (receive (rest sysex-event)
+                  (parse-midi-divided-sysex delta data)
+           (loop (cons sysex-event acc) rest)))
+        (else
+         (error "invalid midi event"))))))
