b3bdfd840294bc4529ce8bca8ecd0a58fd9443f7
rivo <devnull@localhost>
2013-09-20 22:12:13 +0400

    midifile save draft

--- midifile.scm
+++ midifile.scm
@@ -1,9 +1,9 @@
 (module midifile
-  (midifile-load)
+  (midifile-load midifile-save)
   (import scheme chicken)
-  (require-extension bitstring matchable utils)
+  (require-extension bitstring matchable utils posix)
 
-(define-record midifile format time-division tracks)
+(define-record midifile format division tracks)
 
 (define (midifile-load name)
   (bitmatch (read-all name)
@@ -11,6 +11,105 @@
      (parse-midi-format rest))
     ((rest bitstring)
      (error "invalid midi file signature" (failure-location rest)))))
+
+(define (midifile-save mf name)
+  (let ((fd (file-open name (+ open/write open/creat open/trunc))))
+    (file-write fd
+       (bitstring->blob
+        (bitconstruct
+         ("MThd")
+         (#x00000006 32)
+         ((midifile-format mf) 16)
+         ((length (midifile-tracks mf)) 16)
+         ((store-midi-time-division mf) bitstring)
+         ((store-midi-tracks mf) bitstring))))
+    (file-close fd)))
+
+(define (store-midi-time-division mf)
+  (match (midifile-division mf)
+    (('ticks-per-beat ticks)
+     (bitconstruct (0 1) (ticks 15)))
+    (('frames-per-second frames clocks)
+     (bitconstruct (1 1) (frames 7) (clocks 8)))))
+
+(define (store-midi-tracks mf)
+  (let loop ((acc (list))
+             (tracks (midifile-tracks mf)))
+    (match tracks
+      (()
+       (let* ((track-data (apply bitstring-append (reverse acc)))
+              (track-size (quotient (bitstring-length track-data) 8)))
+         (bitconstruct ("MTrk")
+                       (track-size 32)
+                       (track-data bitstring))))
+      ((track . rest)
+       (loop (cons (store-midi-track track) acc) rest)))))
+
+(define (make-status-byte status channel)
+  (bitwise-ior (arithmetic-shift status 4) channel))
+           
+(define (store-midi-track track)
+  (define acc (bitstring-create))
+  (let loop ((running-status 0)
+             (lst track))
+    (match lst
+     (()
+       acc)
+     (((delta 'sysex-event status data) . rest)
+       ; todo: handle divided message
+       (bitstring-append! acc (bitconstruct ((store-variable-length delta) bitstring)
+                                            (status 8)
+                                             ((store-variable-length (blob-size data)) bitstring)
+                                            (data bitstring)))
+       (loop status rest))
+     (((delta 'meta-event type data) . rest)
+       (bitstring-append! acc (bitconstruct ((store-variable-length delta) bitstring)
+                                            (#xFF)
+                                            (type 8)
+                                            ((store-variable-length (blob-size data)) bitstring)
+                                            (data bitstring)))
+       (loop #xFF rest))
+     (((delta midi-event status channel . args) . rest)
+      (let ((status-byte (make-status-byte status channel)))
+        (if (= running-status status-byte)
+          (begin ; omit status byte
+            (bitstring-append! acc (bitconstruct
+                                    ((store-variable-length delta) bitstring)
+                                    ((store-midi-event-data midi-event args) bitstring)))
+            (loop running-status rest))
+          (begin ; setup new running status
+            (bitstring-append! acc (bitconstruct
+                                    ((store-variable-length delta) bitstring)
+                                    (status-byte 8)
+                                    ((store-midi-event-data midi-event args) bitstring)))
+            (loop status-byte rest)))))
+      ((midi-event . rest)
+       (error "unknown midi event" midi-event)))))
+       
+      
+(define (store-midi-event-data id args)
+  (match (cons id args)
+    (('note-on note velocity)
+     (bitconstruct (note 8) (velocity 8)))
+    (('note-off note velocity)
+     (bitconstruct (note 8) (velocity 8)))
+    (('note-aftertouch note value)
+     (bitconstruct (note 8) (value 8)))
+    (('controller number value)
+     (bitconstruct (number 8) (value 8)))
+    (('program-change number)
+     (bitconstruct (number 8) (0 8)))
+    (('channel-aftertouch value)
+     (bitconstruct (value 8) (0 8)))
+    (('pitch-bend value)
+     (bitconstruct (value 16)))))         
+
+(define (store-variable-length len #!optional (acc (list)))
+  (if (and (zero? len) (not (eq? '() acc)))
+    (apply bitstring-append (reverse acc))
+    (store-variable-length (arithmetic-shift len -7)
+                           (cons (bitconstruct (1 1) ((bitwise-and len #x7F) 7))
+                                 acc))))
 
 (define (parse-midi-format data)
   (bitmatch data
@@ -72,19 +171,19 @@
              (parse-midi-status-byte var-data current-status current-channel)
       (bitmatch rest
         (((check (= status #x8)) (note 8) (velocity 8) (rest bitstring))
-         (values (list delta 'note-off channel note velocity) status channel rest))
+         (values (list delta 'note-off status channel note velocity) status channel rest))
         (((check (= status #x9)) (note 8) (velocity 8) (rest bitstring))
-          (values (list delta 'note-on channel note velocity) status channel rest))
+          (values (list delta 'note-on status channel note velocity) status channel rest))
         (((check (= status #xA)) (note 8) (value 8) (rest bitstring))
-         (values (list delta 'note-aftertouch channel note value) status channel rest))
+         (values (list delta 'note-aftertouch status channel note value) status channel rest))
         (((check (= status #xB)) (number 8) (value 8) (rest bitstring))
-         (values (list delta 'controller channel number value) status channel rest))
+         (values (list delta 'controller status channel number value) status channel rest))
         (((check (= status #xC)) (number 8) (unused 8) (rest bitstring))
-         (values (list delta 'program-change channel number) status channel rest))
+         (values (list delta 'program-change status channel number) status channel rest))
         (((check (= status #xD)) (value 8) (unused 8) (rest bitstring))
-         (values (list delta 'channel-aftertouch channel value) status channel rest))
+         (values (list delta 'channel-aftertouch status channel value) status channel rest))
         (((check (= status #xE)) (value 16) (rest bitstring))
-         (values (list delta 'pitch-bend channel value) status channel rest))
+         (values (list delta 'pitch-bend status channel value) status channel rest))
         (((check (= status #xFF)) (type 8) (event-data bitstring))
          (receive (meta-event rest)
                   (parse-midi-meta-event delta type event-data)
@@ -126,7 +225,7 @@
            (parse-variable-length event-data 0)
     (bitmatch var-data
       (((data (* len 8) bitstring) (rest bitstring))
-       (values (list delta 'sysex-event (bitstring->blob data)) rest))
+       (values (list delta 'sysex-event #xF7 (bitstring->blob data)) rest))
       (else
        (error "invalid authorization sysex event" (failure-location var-data))))))
 
@@ -140,7 +239,7 @@
       (bitmatch var-data
         (((data (* len 8) bitstring) (#xF7) (rest bitstring))
          ; normal message
-         (values (list delta 'sysex-event (concat-sysex-data (cons data acc))) rest))
+         (values (list delta 'sysex-event #xF0 (concat-sysex-data (cons data acc))) rest))
         (((data (* len 8) bitstring) (#xF0) (rest bitstring))
          ; divided message
          (loop (cons data acc) rest))
